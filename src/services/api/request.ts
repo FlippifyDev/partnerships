5b5a5d3d39e925ac5a91a758862e9daad4fb5f3a
RoyalGr4pe <njames.programming@gmail.com>
2025-04-18 17:08:39 +0100

    fix orgin in requests

--- src/services/api/request.ts
+++ src/services/api/request.ts
@@ -1,42 +1,11 @@
 import { IFirebaseConfig } from "@/models/config";
 
-// Function to send request and handle token refresh if necessary
-export async function updateStoreInfo(endpoint: string, ebayAccessToken: string, uid: string): Promise<void> {
-    // Set up headers with the access token
-    const headers = {
-        Authorization: `Bearer ${ebayAccessToken}`,
-        "Accept": "*/*",
-        "Origin": "https://flippify.io",
-        "Referer": "https://flippify.io/",
-        "Content-Type": "application/json",
-        "Sec-Fetch-Mode": "cors",
-        "Sec-Fetch-Site": "same-site",
-    };
-
-    const url = new URL(`https://api.flippify.io/v1/${endpoint}`);
-    url.searchParams.append("uid", uid);
-
-    try {
-        const response = await fetch(url, {
-            method: "POST",
-            headers: headers
-        });
-
-        if (!response.ok) {
-            throw new Error(`Failed to update store info | ${response.status} - ${response.statusText}`);
-        }
-    } catch (error) {
-        console.error(error)
-    }
-}
-
-
 export async function retrieveStatus(): Promise<IFirebaseConfig | void> {
     // Set up headers with the access token
     const headers = {
         "Accept": "*/*",
-        "Origin": "https://flippify.io",
-        "Referer": "https://flippify.io/",
+        "Origin": "https://partnerships.flippify.io",
+        "Referer": "https://partnerships.flippify.io/",
         "Content-Type": "application/json",
         "Sec-Fetch-Mode": "cors",
         "Sec-Fetch-Site": "same-site",
